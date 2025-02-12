f07c54a10e7300538e991029456bbbc450194d25
東門　博飛 <s1422162@mail.meio-u.ac.jp>
2025-02-13 06:31:11 +0900

    sitemap

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -9,4 +11,40 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+// ビルド時にサイトマップを生成
+const generateSitemap = () => {
+  const today = new Date().toISOString().split('T')[0];
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://31meiofes.vercel.app</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://31meiofes.vercel.app/news</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://31meiofes.vercel.app/timetable</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>`;
+
+  fs.writeFileSync('public/sitemap.xml', sitemap);
+};
+
+module.exports = {
+  ...nextConfig,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      generateSitemap();
+    }
+    return config;
+  },
+};
